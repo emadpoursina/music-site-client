e136c96901a1dcab037458ce86477df845070901
emadpoursina <emad.puursina@gmail.com>
2025-06-30 23:09:34 +0330

    add logo picture

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,5 @@
 import SearchInput from "./SearchInput";
+import logo from "../assets/react.svg";
 
 const TopBar = () => (
   <header className="w-full flex items-center justify-between px-4 py-2 border-b border-gray-200 bg-white">
@@ -9,7 +10,7 @@ const TopBar = () => (
       <SearchInput />
     </div>
     <div className="w-24 flex items-center justify-end">
-      <span className="font-bold text-lg">logo</span>
+      <img src={logo} alt="React Logo" className="h-8 w-auto" />
     </div>
   </header>
 );
