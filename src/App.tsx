2b8c257bab74ae623b5fd8a94b2c6da201c09f9f
emadpoursina <emad.puursina@gmail.com>
2025-07-02 20:21:13 +0330

    add routing singer with /singer route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import SingerList from "./components/SingerList";
 
 function App() {
   return (
     <Layout>
-      <div className="w-full h-full flex items-center justify-center text-2xl text-gray-400">
-        Main Content
-      </div>
+      <Routes>
+        <Route path="/singer" element={<SingerList />} />
+        <Route
+          path="/"
+          element={
+            <div className="w-full h-full flex items-center justify-center text-2xl text-gray-400">
+              صفحه اصلی
+            </div>
+          }
+        />
+      </Routes>
     </Layout>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,15 @@
 const Sidebar = () => (
   <aside className="w-48 bg-gray-50 border-l border-gray-200 p-4 flex flex-col gap-4 rounded-l-2xl mt-4 mb-4 mr-4">
     {[...Array(5)].map((_, i) => (
-      <div key={i} className="h-10 bg-gray-200 rounded-md mb-2 last:mb-0" />
+      <a key={i} className="h-10 bg-gray-200 rounded-md mb-2 last:mb-0"></a>
     ))}
+    <a
+      href="/singer"
+      key={6}
+      className="h-10 bg-gray-200 rounded-md mb-2 last:mb-0"
+    >
+      Singer
+    </a>
   </aside>
 );
 
